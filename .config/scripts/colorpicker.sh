99efea14f819eca928e714afb5ea1c775f3ed106
Hlib Lukavetskyi <lukavetskyihlib@gmail.com>
2025-12-13 16:59:10 +0100

    add scripts

---
+++ .config/scripts/colorpicker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xcolor | xclip -sel clip
+
---
+++ .config/scripts/wpowermenu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+options=" ⏻ \n  \n  "
+
+chosen=$(echo -e "$options" | wofi -c ~/.config/wofi/power.conf -s ~/.config/wofi/power.css --dmenu --title "Power Menu")
+
+case $chosen in
+    " ⏻ ")
+        shutdown now
+        ;;
+    "  ")
+        reboot
+        ;;
+    "  ")
+        hyprlock 
+        ;;
+    *)
+        exit 0
+        ;;
+esac
