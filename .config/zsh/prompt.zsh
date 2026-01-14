b7fddc1915d6e49bf0010e6fe0225b034d356d2c
Hlib Lukavetskyi <lukavetskyihlib@gmail.com>
2026-01-14 12:19:09 +0100

    git branch in zsh

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,2 +1,17 @@
-PROMPT="%F{yellow}[%D{%H:%M}]%f %F{green}[%~]%f
+parse_git_branch() {
+  if git rev-parse --is-inside-work-tree &>/dev/null; then
+      git branch 2>/dev/null | sed -n 's/^\* \(.*\)/ [\1]/p'
+    else
+      echo ""
+    fi
+}
+
+custom_prompt(){
+  local git_branch=$(parse_git_branch)
+  PROMPT="%F{yellow}[%D{%H:%M}]%f%F{magenta}$git_branch%f %F{green}[%~]%f
 %(?:%F{blue}» :%F{red}»%f "
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd custom_prompt
+
